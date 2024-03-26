35d1afe909bb1096fd27d0fbe21aa60853c97710
Haytham Arihi <arihihaytham01@gmail.com>
2024-03-26 14:34:14 +0100

    Add ShuffleArray function in script.js + edit README.md

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 Find here the used resources for my portfolio.
 ### script.js
-function getPokemonName
-sources: Coding along - S2E2 - Steps 1 to 3 by Mike Derycke
+## function getPokemonName
+Gebruiken van een constante om data te bewaren uit een API
+JSON manipuleren en weergeven door data te halen uit response
+    Coding along - S2E2 - Steps 1 to 3 by Mike Derycke
+    https://www.youtube.com/watch?v=Q13QGyTqePs
+## shuffleArray
+
+    How to Fisher-Yates shuffle a JavaScript array? asked by loadmasterbob
+    https://stackoverflow.com/questions/59810241/how-to-fisher-yates-shuffle-a-javascript-array
--- script/script.js
+++ script/script.js
@@ -1,15 +1,24 @@
-(async function getPokemonNames() {
+async function getPokemonNames() {
     try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1017');
         const data = await response.json();
         let i=1;
-        
+        const shuffledArray = shuffleArray(data.results);
+        const firstSix = shuffledArray.slice(0,6);
         // List of pokémon with as format "number. name"
-        data.results.forEach((element) => {
-                console.log(`${i}. ${element.name}`);  
+        firstSix.forEach((element) => {
+                console.log(`${i}. ${element.url}`);  
                 i++;  
         });
     } catch (error) {
         console.error('Error fetching data:', error);
     }
-})
+}
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+getPokemonNames();
