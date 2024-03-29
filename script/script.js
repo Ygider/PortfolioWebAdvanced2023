20396bbff059fcb14cf4d21ab3b34936f5e8d447
Haytham Arihi <arihihaytham01@gmail.com>
2024-03-29 16:35:29 +0100

    Cleaning function getPokemonNames and getPokemonSpriteAndType

--- script/script.js
+++ script/script.js
@@ -7,22 +7,17 @@ async function getPokemonNames() {
         const firstSix = shuffledArray.slice(0,6);
         // List of pokémon with as format "number. name"
         for (const element of firstSix) {
-            console.log(`${i}. ${element.name}`);
-            const url = element.url;
-            const response2 = await fetch(url);
-            const data2 = await response2.json();
-            const sprite = await getPokemonSpriteAndType(element.url);
-            const type = data2.types.length;
-            let types = [];
-            if (type === 1) {
-                //console.log("Type: ", data2.types[0].type.name);
-                types.push(data2.types[0].type.name);
-            } else {   
-                //console.log("Type1: ", data2.types[0].type.name, "Type2:", data2.types[1].type.name );
-                types.push(data2.types[0].type.name);
-                types.push(data2.types[1].type.name);
+            let pokemon = {
+                name: '',
+                sprite: '',
+                type: []
             }
-            console.log(types);
+            pokemon.name = element.name
+            const url = element.url;
+            const {sprite, types} = await getPokemonSpriteAndType(url)
+            pokemon.sprite = sprite
+            pokemon.type = types
+            console.log(pokemon)
             i++;
         }
     } catch (error) {
@@ -41,14 +36,19 @@ async function getPokemonSpriteAndType(url){
         const response = await fetch(url);
         const data = await response.json();
         const sprite= data.sprites.other["official-artwork"].front_default;
-        console.log(sprite);
-        //const type = data.types;
-        //console.log(type.length);
-        return sprite;
+
+        const type = data.types.length
+        let types = [];
+        if (type === 1) {
+            types.push(data.types[0].type.name);
+        } else {
+            types.push(data.types[0].type.name);
+            types.push(data.types[1].type.name);
+        }
+
+        return {sprite, types}
     } catch (error) {
         console.error('Error fetching data:', error);
     }
-    
-
 }
 getPokemonNames();
