029755633fb10285144edc12b1d4db164e893872
Haytham Arihi <arihihaytham01@gmail.com>
2024-03-29 15:26:19 +0100

    Add function getPokemonSpriteAndType and edit function getPokemonName so he calls the function getPokemonSpriteAndType

--- script/script.js
+++ script/script.js
@@ -6,10 +6,25 @@ async function getPokemonNames() {
         const shuffledArray = shuffleArray(data.results);
         const firstSix = shuffledArray.slice(0,6);
         // List of pokémon with as format "number. name"
-        firstSix.forEach((element) => {
-                console.log(`${i}. ${element.url}`);  
-                i++;  
-        });
+        for (const element of firstSix) {
+            console.log(`${i}. ${element.name}`);
+            const url = element.url;
+            const response2 = await fetch(url);
+            const data2 = await response2.json();
+            const sprite = await getPokemonSpriteAndType(element.url);
+            const type = data2.types.length;
+            let types = [];
+            if (type === 1) {
+                //console.log("Type: ", data2.types[0].type.name);
+                types.push(data2.types[0].type.name);
+            } else {   
+                //console.log("Type1: ", data2.types[0].type.name, "Type2:", data2.types[1].type.name );
+                types.push(data2.types[0].type.name);
+                types.push(data2.types[1].type.name);
+            }
+            console.log(types);
+            i++;
+        }
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -20,5 +35,20 @@ function shuffleArray(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
+}
+async function getPokemonSpriteAndType(url){
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const sprite= data.sprites.other["official-artwork"].front_default;
+        console.log(sprite);
+        //const type = data.types;
+        //console.log(type.length);
+        return sprite;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+    
+
 }
 getPokemonNames();
